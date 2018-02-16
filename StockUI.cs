921568eae67bee306251953a32f4b877c5651f9f
JOB <getabujob@gmail.com>
2018-02-16 04:42:47 +0300

    Stock Logic in place

--- StockUI.cs
+++ StockUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using custom_alert_notifications;
 
 namespace Kimathi_Construction
 {
@@ -36,6 +37,8 @@ namespace Kimathi_Construction
 
             //load up the gui
             LoadUpGui();
+
+           
         }
 
         private async void LoadUpGui()
@@ -51,20 +54,23 @@ namespace Kimathi_Construction
 
                 var temp1 = stoklist.Where(x => x.RawMaterial.Equals("cement"));
                 var temp2 = stoklist.Where(x => x.RawMaterial.Equals("water"));
-                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("brick"));
+                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("bricks"));
 
                 tbcementreg.Text = temp1.FirstOrDefault().AmountIn.ToString();
                 tbwaterreg.Text = temp2.FirstOrDefault().AmountIn.ToString();
-                tbbrickreg.Text = temp1.FirstOrDefault().AmountIn.ToString();
+                tbbrickreg.Text = temp3.FirstOrDefault().AmountIn.ToString();
 
                 //notifications
                 Notifications();
 
                 //remaining
-                lbltu.Text = temp1.FirstOrDefault().Quantity.ToString() +" Kg";
+                lblQty1.Text = temp1.FirstOrDefault().Quantity.ToString() + " Kg";
                 lblWtr.Text = temp2.FirstOrDefault().Quantity.ToString() + " ltr";
                 lblBricks.Text = temp3.FirstOrDefault().Quantity.ToString() + " pieces";
 
+                btnSave1.Visible = false;
+                btnSave2.Visible = false;
+                btnSave3.Visible = false;
             }
         }
 
@@ -81,11 +87,11 @@ namespace Kimathi_Construction
 
                 var temp1 = stoklist.Where(x => x.RawMaterial.Equals("cement"));
                 var temp2 = stoklist.Where(x => x.RawMaterial.Equals("water"));
-                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("brick"));
+                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("bricks"));
 
                 tbcementreg.Text = temp1.FirstOrDefault().AmountIn.ToString();
                 tbwaterreg.Text = temp2.FirstOrDefault().AmountIn.ToString();
-                tbbrickreg.Text = temp1.FirstOrDefault().AmountIn.ToString();
+                tbbrickreg.Text = temp3.FirstOrDefault().AmountIn.ToString();
 
                 //cement
                 if (temp1.FirstOrDefault().Quantity < 50)
@@ -108,6 +114,7 @@ namespace Kimathi_Construction
                     not1.Text = "No Usable Cement"; //Low Cement
                     not1.ForeColor = Color.Crimson;
                 }
+
                 //water
                 if (temp2.FirstOrDefault().Quantity < 500)
                 {
@@ -133,7 +140,7 @@ namespace Kimathi_Construction
                 //bricks
                 if (temp3.FirstOrDefault().Quantity < 50)
                 {
-                    not3.Text = "Less Bricks"; //Low Cement
+                    not3.Text = "Few Bricks"; //Low Cement
                     not3.ForeColor = Color.Crimson;
                 }
                 else
@@ -198,22 +205,254 @@ namespace Kimathi_Construction
             lbltu3.Visible = false;
         }
 
-        private void btnSave1_Click(object sender, EventArgs e)
+        private async void btnSave1_Click(object sender, EventArgs e)
         {
+
+            if (string.IsNullOrEmpty(tbcementreg.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Cement Registered !", alert.AlertType.warnig);
+                return;
+            }
+            if (string.IsNullOrEmpty(tbcementused.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Cement Used !", alert.AlertType.warnig);
+                return;
+            }
+
             btnSave1.Visible = false;
             lbltu.Visible = true;
+
+            using (var context = new KimathiEntities())
+            {
+                var stoklist = await Task.Factory.StartNew(() =>
+                {
+                    return context.Stocks
+                    .OrderBy(x => x.Id)
+                    .ToList();
+                });
+
+                var temp1 = stoklist.Where(x => x.RawMaterial.Equals("cement"));
+                var temp2 = stoklist.Where(x => x.RawMaterial.Equals("water"));
+                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("brick"));
+
+                
+
+                try
+                {
+                    //check if required quantity has changed
+                    if (int.Parse(tbcementreg.Text) == temp1.FirstOrDefault().AmountIn)
+                    {
+                        temp1.FirstOrDefault().AmountOut += int.Parse(tbcementused.Text);
+                        temp1.FirstOrDefault().Quantity = temp1.FirstOrDefault().AmountIn - temp1.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp1.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        //now update 
+                        temp1.FirstOrDefault().AmountIn = int.Parse(tbcementreg.Text);
+                        temp1.FirstOrDefault().AmountOut += int.Parse(tbcementused.Text);
+                        temp1.FirstOrDefault().Quantity = temp1.FirstOrDefault().AmountIn - temp1.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp1.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+
+                }
+                catch (Exception) { }
+
+            }
         }
 
-        private void btnSave2_Click(object sender, EventArgs e)
+        private async void btnSave2_Click(object sender, EventArgs e)
         {
+           
+
+            if (string.IsNullOrEmpty(tbwaterreg.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Water Registered !", alert.AlertType.warnig);
+                return;
+            }
+            if (string.IsNullOrEmpty(tbwaterused.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Water Used !", alert.AlertType.warnig);
+                return;
+            }
+
             btnSave2.Visible = false;
             lbltu2.Visible = true;
+
+
+            using (var context = new KimathiEntities())
+            {
+                var stoklist = await Task.Factory.StartNew(() =>
+                {
+                    return context.Stocks
+                    .OrderBy(x => x.Id)
+                    .ToList();
+                });
+
+                var temp1 = stoklist.Where(x => x.RawMaterial.Equals("cement"));
+                var temp2 = stoklist.Where(x => x.RawMaterial.Equals("water"));
+                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("brick"));
+
+
+
+                try
+                {
+                    //check if required quantity has changed
+                    if (int.Parse(tbwaterreg.Text) == temp2.FirstOrDefault().AmountIn)
+                    {
+                        temp2.FirstOrDefault().AmountOut += int.Parse(tbwaterused.Text);
+                        temp2.FirstOrDefault().Quantity = temp2.FirstOrDefault().AmountIn - temp2.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp2.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        //now update 
+                        temp2.FirstOrDefault().AmountIn = int.Parse(tbwaterreg.Text);
+                        temp2.FirstOrDefault().AmountOut += int.Parse(tbwaterused.Text);
+                        temp2.FirstOrDefault().Quantity = temp2.FirstOrDefault().AmountIn - temp1.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp2.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+
+                }
+                catch (Exception) { }
+
+            }
         }
 
-        private void btnSave3_Click(object sender, EventArgs e)
+        private async void btnSave3_Click(object sender, EventArgs e)
         {
+           
+
+            if (string.IsNullOrEmpty(tbbrickreg.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Brick Registered !", alert.AlertType.warnig);
+                return;
+            }
+            if (string.IsNullOrEmpty(tbbrickused.Text))
+            {
+                //UI code  
+
+                alert.Show("Required info \n Brick Used !", alert.AlertType.warnig);
+                return;
+            }
+
             btnSave3.Visible = false;
             lbltu3.Visible = true;
+
+            using (var context = new KimathiEntities())
+            {
+                var stoklist = await Task.Factory.StartNew(() =>
+                {
+                    return context.Stocks
+                    .OrderBy(x => x.Id)
+                    .ToList();
+                });
+
+                var temp1 = stoklist.Where(x => x.RawMaterial.Equals("cement"));
+                var temp2 = stoklist.Where(x => x.RawMaterial.Equals("water"));
+                var temp3 = stoklist.Where(x => x.RawMaterial.Equals("brick"));
+
+
+
+                try
+                {
+                    //check if required quantity has changed
+                    if (int.Parse(tbbrickreg.Text) == temp3.FirstOrDefault().AmountIn)
+                    {
+                        temp3.FirstOrDefault().AmountOut += int.Parse(tbbrickused.Text);
+                        temp3.FirstOrDefault().Quantity = temp2.FirstOrDefault().AmountIn - temp3.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp3.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        //now update 
+                        temp3.FirstOrDefault().AmountIn = int.Parse(tbbrickreg.Text);
+                        temp3.FirstOrDefault().AmountOut += int.Parse(tbbrickused.Text);
+                        temp3.FirstOrDefault().Quantity = temp3.FirstOrDefault().AmountIn - temp3.FirstOrDefault().AmountOut;
+
+                        try
+                        {
+                            context.Entry<Stock>(temp2.FirstOrDefault()).State = EntityState.Modified;
+                            context.SaveChanges();
+                            alert.Show("Updated !", alert.AlertType.success);
+                            LoadUpGui();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+
+                }
+                catch (Exception) { }
+
+            }
+
         }
     }
 }
